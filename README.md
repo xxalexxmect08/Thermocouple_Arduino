ab85b424f4a2fe5b3c60992defc3eb0764dcaeed
xxalexxmect08 <xxalexxmect08@users.noreply.github.com>
2019-01-31 12:04:33 +0100

    Updated Project
    Updated the Arduino Code, the KiCad Project, Avanzamento Attività and README

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Thermocouple_Arduino
 
-Using a Thermocouple K and a MAX6675 converter
+Using a Thermocouple K, a MAX6675 converter and a DHT11
 
-Lezione utlizizzando i componenti sopra citati per testare l'acquisizione della temperatura 
-e scrivendoli con un intervallo di 30 secondi attraverso una porta seriale
+Lezione utlizizzando i componenti sopra citati per testare l'acquisizione della temperatura e umidità relativa
+e scrivendoli con un intervallo di 10 secondi attraverso una porta seriale
 
 
 
---
+++ serialthermocouple/serialthermocouple.ino
@@ -0,0 +1,84 @@
+// this example is public domain. enjoy!
+// www.ladyada.net/learn/sensors/thermocouple
+
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#include "max6675.h"
+unsigned long old_millis = 0;
+unsigned long delta = 10000;
+int minuti = 1;
+int ledPin = 8;
+int ledMax = 12;
+int thermoDO = 4;
+int thermoCS = 5;
+int thermoCLK = 6;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+
+
+void setup() {
+  Serial.begin(9600);
+  // use Arduino pins
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledMax, OUTPUT);
+
+  dht.begin();
+
+  // wait for MAX chip to stabilize
+  delay(500);
+}
+
+void loop() {
+
+  delay(2000);
+
+  unsigned long now = millis();
+  float T_thermo = thermocouple.readCelsius();
+
+  if (now > old_millis + delta) {
+    Serial.print("Termocoppia: ");
+    //Serial.print(minuti);
+    //Serial.print(" , ");
+    Serial.print(T_thermo);
+    Serial.println("°C ");
+    old_millis = now;
+    minuti = minuti + 1;
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+  }
+  float h = dht.readHumidity();
+  float T_DHT11 = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(T_DHT11, h, false);
+
+  if (isnan(h) || isnan(T_DHT11) || isnan(f)) {
+    Serial.println(F("DHT11: Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print("DHT11: ");
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(T_DHT11);
+  Serial.print(F("°C "));
+  Serial.print(F("  Heat index: "));
+  Serial.print(hic);
+  Serial.println(F("°C "));
+
+  if (T_thermo < 40 && T_thermo > 35) {
+    digitalWrite(ledMax, HIGH);
+  }
+  if (T_thermo > 40 || T_thermo < 35) {
+    digitalWrite(ledMax, LOW);
+  }
+
+  delay(1);
+}
