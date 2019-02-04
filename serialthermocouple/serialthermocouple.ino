c1a448b2edb10021ff1f3437d4da0a22942bf818
xxalexxmect08 <xxalexxmect08@users.noreply.github.com>
2019-02-04 14:41:59 +0100

    Continuazione attività

--- serialthermocouple/serialthermocouple.ino
+++ serialthermocouple/serialthermocouple.ino
@@ -8,7 +8,7 @@ DHT dht(DHTPIN, DHTTYPE);
 
 #include "max6675.h"
 unsigned long old_millis = 0;
-unsigned long delta = 10000;
+unsigned long delta = 1000;
 int minuti = 1;
 int ledPin = 8;
 int ledMax = 12;
@@ -29,6 +29,8 @@ void setup() {
 
   dht.begin();
 
+  Serial.println("Sec,Termocoppia °C,umidità,DHT11 °C,temp percepita °C");
+
   // wait for MAX chip to stabilize
   delay(500);
 }
@@ -37,21 +39,31 @@ void loop() {
 
   delay(2000);
 
-  unsigned long now = millis();
+    unsigned long now = millis();
   float T_thermo = thermocouple.readCelsius();
 
   if (now > old_millis + delta) {
-    Serial.print("Termocoppia: ");
-    //Serial.print(minuti);
+    //Serial.print("Termocoppia: ");
+    Serial.print(minuti);
+    Serial.print(",");
     //Serial.print(" , ");
     Serial.print(T_thermo);
-    Serial.println("°C ");
+    Serial.print(",");
+    //Serial.print(" °C ");
     old_millis = now;
     minuti = minuti + 1;
     digitalWrite(ledPin, HIGH);
     delay(500);
     digitalWrite(ledPin, LOW);
   }
+
+  if (T_thermo < 40 && T_thermo > 35) {
+    digitalWrite(ledMax, HIGH);
+  }
+  if (T_thermo > 40 || T_thermo < 35) {
+    digitalWrite(ledMax, LOW);
+  }
+
   float h = dht.readHumidity();
   float T_DHT11 = dht.readTemperature();
   float f = dht.readTemperature(true);
@@ -63,22 +75,18 @@ void loop() {
     return;
   }
 
-  Serial.print("DHT11: ");
-  Serial.print(F("Humidity: "));
+  //Serial.print("DHT11: ");
+  //Serial.print(F("Humidity: "));
   Serial.print(h);
-  Serial.print(F("%  Temperature: "));
+  Serial.print(",");
+  //Serial.print(F(" %  Temperature: "));
   Serial.print(T_DHT11);
-  Serial.print(F("°C "));
-  Serial.print(F("  Heat index: "));
-  Serial.print(hic);
-  Serial.println(F("°C "));
+  Serial.print(",");
+  //Serial.print(F(" °C "));
+  //Serial.print(F("  Heat index: "));
+  Serial.println(hic);
+  //Serial.println(F(" °C "));
 
-  if (T_thermo < 40 && T_thermo > 35) {
-    digitalWrite(ledMax, HIGH);
-  }
-  if (T_thermo > 40 || T_thermo < 35) {
-    digitalWrite(ledMax, LOW);
-  }
 
   delay(1);
 }
